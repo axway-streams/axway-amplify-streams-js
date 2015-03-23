dc4ce1c858ec9f86ea7614e5aafc974615e7dc1c
Raphaël LEO <skeptikos@hotmail.com>
2015-03-23 17:02:28 +0100

    Call any URL and show updated cells on jQuery sample

--- stockmarket/app/scripts/main.js
+++ stockmarket/app/scripts/main.js
@@ -1,138 +1,138 @@
 /* jshint devel:true */
 (function() {
 
-var streamdata = null;
-var stocks = [];
+    var streamdata = null;
+    var stocks = [];
 
-function init() {
-  $('#connect').on('click', connect);
-  $('#disconnect').on('click', disconnect);
+    function init() {
+        $('#connect').on('click', connect);
+        $('#disconnect').on('click', disconnect);
+        $('#disconnect').attr('disabled', 'disabled');
 
-  $(document).ready(function() {
-      $('#inputUrl').keyup(function() {
+        $(document).ready(function() {
+            $('#inputUrl').keyup(function() {
+                var empty = false;
+                $('#inputUrl').each(function() {
+                    if ($(this).val().length == 0) {
+                        empty = true;
+                    }
+                });
 
-          var empty = false;
-          $('#inputUrl').each(function() {
-              if ($(this).val().length == 0) {
-                  empty = true;
-              }
-          });
+                if (empty) {
+                    $('#connect').attr('disabled', 'disabled');
+                    console.log('empty');
+                } else {
+                    $('#connect').removeAttr('disabled');
+                    console.log('not empty');
+                }
+            });
+        });
+    };
 
-          if (empty) {
-              $('#connect').attr('disabled', 'disabled');
-              console.log('empty');
-          } else {
-              $('#connect').removeAttr('disabled');
-              console.log('not empty');
-          }
-      });
+    function connect() {
+        var url = $('#inputUrl').val();
+        var headerValue = $.trim($('#inputHeader').val());
+        var header = [];
 
-  });
-};
+        if (headerValue !== null && headerValue.length > 0) {
+            header = [headerValue];
+        }
 
-function connect() {
-  var url = $('#inputUrl').val();
-  var headerValue = $.trim($('#inputHeader').val());
-  var header = [];
+        // create the Streamdata source
+        streamdata = streamdataio.createEventSource(url, header);
 
-  if (headerValue !== null && headerValue.length > 0) {
-    header = [headerValue];
+        streamdata.streamdataConfig.PROTOCOL = 'https://';
+        streamdata.streamdataConfig.HOST = 'proxy.streamdata.io';
+        streamdata.streamdataConfig.PORT = '';
 
-  }
+        // add a callback when the connection is opened
+        streamdata.onOpen(function() {
+            $('#connect').attr('disabled', 'disabled');
+            $('#disconnect').removeAttr('disabled');
+        });
 
-  // create the Streamdata source
-  streamdata = streamdataio.createEventSource(url, header);
+        // add a callback when data is sent by streamdata.io
+        streamdata.onData(function(data) {
+            stocks = data;
+            renderStocks([]);
+        });
 
-  streamdata.streamdataConfig.PROTOCOL = 'https://';
-  streamdata.streamdataConfig.HOST = 'proxy.streamdata.io';
-  streamdata.streamdataConfig.PORT = '';
+        // add a callback when a patch is sent by streamdata.io
+        streamdata.onPatch(function(patch) {
+            // apply the json-patch to the array of values
+            jsonpatch.apply(stocks, patch);
 
-  // add a callback when the connection is opened
-  streamdata.onOpen(function() {
-    $('#connect').attr('disabled', 'disabled');
-    $('#disconnect').removeAttr('disabled');
-  });
+            // add areNew property to highlight changes
+            stocks.map(function(item, index) {
+                item.areNew = new Array();
+            });
+            patch.forEach(function(item) {
+                var index = parseInt(item.path.substring(1, item.path.indexOf('/', 1)));
+                var attribute = item.path.substring(item.path.indexOf('/', 1) + 1);
+                // we keep only first level attribute
+                if(attribute.indexOf('/') != -1) attribute = attribute.substring(0,attribute.indexOf('/'));
+                switch(item.op) {
+                    case "add":
+                    case "replace":
+                        stocks[index].areNew.push(attribute);
+                        break;
+                }
+            });
 
-  // add a callback when data is sent by streamdata.io
-  streamdata.onData(function(data) {
-    stocks = data;
-    renderStocks([]);
-  });
+            renderStocks();
+        });
 
-  // add a callback when a patch is sent by streamdata.io
-  streamdata.onPatch(function(patch) {
-    // apply the json-patch to the array of values
-    jsonpatch.apply(stocks, patch);
+        // add a callback when an error occured
+        streamdata.onError(function(error) {
+            console.error('Received an error: ' + error.message);
 
-    var indexes = patch.map(function(item) {
-      var path = item.path;
-      var idx = path.indexOf('/', 1);
-      return parseInt(path.substring(1, idx));
-    });
+            disconnect();
 
-    renderStocks(indexes);
-  });
+            $('#error').append(error.message);
+            $('#error').removeClass('hide');
 
-  // add a callback when an error occured
-  streamdata.onError(function(error) {
-    console.error('Received an error: ' + error.message);
+            setTimeout(function() {
+                $('#error').empty();
+                $('#error').addClass('hide');
+            }, 5000);
+        });
 
-    disconnect();
+        streamdata.open();
+    };
 
-    $('#error').append(error.message);
-    $('#error').removeClass('hide');
-
-    setTimeout(function() {
-       $('#error').empty();
-       $('#error').addClass('hide');
-
-    }, 5000);
-  });
-
-  streamdata.open();
-};
-
-function disconnect() {
-  streamdata.close();
-  $('#disconnect').attr('disabled', 'disabled');
-  $('#connect').removeAttr('disabled');
-};
+    function disconnect() {
+        streamdata.close();
+        $('#disconnect').attr('disabled', 'disabled');
+        $('#connect').removeAttr('disabled');
+    };
 
 
-function renderStocks(updates) {
-  updates = updates || [];
+    function renderStocks() {
 
-  if (updates.length === 0) {
-    // call from onData(.): first data sent
-    $('#stocks tbody').empty();
+        $('#stocks').empty();
 
-  }
+        // create header
+        $('#stocks').append("<thead><tr></tr></thead");
+        $.each(stocks[0], function(index, value) {
+            // do not display areNew attribute
+            if(index != "areNew") {
+                $('#stocks thead tr').append('<td>' + index + '</td>');
+            }
+        });
+        // create body
+        $('#stocks').append("<tbody></tbody>");
+        $.each(stocks, function(lineIndex, lineValue) {
+            $('#stocks tbody').append("<tr></tr>");
+            $.each(lineValue, function(cellIndex, cellValue) {
+                var classNew = (lineValue.areNew && lineValue.areNew.indexOf(cellIndex) != -1) ? "success" : "";
+                // do not display areNew attribute
+                if(cellIndex != "areNew") {
+                    $('#stocks tbody tr:last').append('<td class="' + classNew + '">' + cellValue + '</td>');
+                }
+            });
+        });
+    };
 
-  for (i = 0, l = stocks.length; i < l; i++) {
-   if ($.inArray(i, updates) === -1) {
-      var stock = $('#stocks #' + i);
-      if (stock.length === 0) {
-        $('#stocks').append('<tr id="' + i + '">'
-                 + '<td>' + stocks[i].title + '</td>'
-                 + '<td>' + stocks[i].price + '</td>'
-                 + '</tr>');
-
-      } else {
-        stock.removeClass('success');
-
-      }
-
-   } else {
-      $('#stocks #' + i).replaceWith(
-          '<tr id="' + i + '" class="success">'
-            + '<td>' + stocks[i].title + '</td>'
-            + '<td>' + stocks[i].price + '</td>'
-            + '</tr>'
-      );
-   }
-  }
-};
-
-init();
+    init();
 
 })();
