3599303fa101b0c9bf7b817ea3408a19a63f10dd
loriepisicchio <lorie.pisicchio@streamdata.io>
2017-07-12 15:55:11 +0200

    Remove duplicated theme declaration

--- stockmarket-vuejs/src/main.js
+++ stockmarket-vuejs/src/main.js
@@ -5,17 +5,11 @@ import App from './App'
 
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
-Vue.use(VueMaterial)
-Vue.material.registerTheme('default', {
-  primary: 'blue',
-  accent: 'red',
-  warn: 'red',
-  background: 'grey'
-})
-
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(VueMaterial)
 Vue.use(BootstrapVue)
 Vue.material.registerTheme('default', {
   primary: 'deep-purple',
